ee94573fe2672033035361b405c225ae83487948
emilabz <emilabrahamz@gmail.com>
2019-01-31 20:48:30 -0500

    changes made in logic

--- src/com/lambton/magicalcardgame/MagicalCardGameCalculator.java
+++ src/com/lambton/magicalcardgame/MagicalCardGameCalculator.java
@@ -7,14 +7,16 @@ public class MagicalCardGameCalculator implements MagicalCardGameInterface{
     public String[][] getFirstShuffleResult(MagicalCardGameModel magicalCardGameModel){
         String[][] a=magicalCardGameModel.getCardList();
         String[][] b=null;
-        if(magicalCardGameModel.getFirstShuffleColPos()=="C1"){
+        //if(magicalCardGameModel.getFirstShuffleColPos()=="C1"){
+        if(magicalCardGameModel.getFirstShuffleColPos().equals("C1")){
             for(i=0;i<3;i++){
                 b[0][i]=a[i][1];
                 b[1][i]=a[i][0];
                 b[2][i]=a[i][2];
             }
         }
-        else if(magicalCardGameModel.getFirstShuffleColPos()=="C2"){
+        //else if(magicalCardGameModel.getFirstShuffleColPos()=="C2"){
+        else if(magicalCardGameModel.getFirstShuffleColPos().equals("C2")){
             for(i=0;i<3;i++){
                 b[0][i]=a[i][0];
                 b[1][i]=a[i][1];
