a4be44c0acdd3eee4cb89544dee87d33385246b9
emilabz <emilabrahamz@gmail.com>
2019-01-31 19:18:14 -0500

    package refraction #4

--- src/com/lambton/magicalcardgame/MCardGame.java
+++ src/com/lambton/magicalcardgame/MCardGame.java
@@ -3,7 +3,6 @@ import java.lang.*;
 public class MCardGame {
     Scanner sc=new Scanner(System.in);
     MagicalCardGameModel magicalCardGameModel=new MagicalCardGameModel();
-
     System.out.println("Welcome to Magical Card Game\nSelect a card")
 
 
