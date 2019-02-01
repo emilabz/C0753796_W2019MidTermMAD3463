7471d41624566ad649e4d50dcad4ee8bf0dd5fd4
emilabz <emilabrahamz@gmail.com>
2019-01-31 19:15:46 -0500

    modified #3

--- src/MCardGame.java
+++ src/MCardGame.java
@@ -1,3 +1,10 @@
 import java.util.Scanner;
+import java.lang.*;
 public class MCardGame {
+    Scanner sc=new Scanner(System.in);
+    MagicalCardGameModel magicalCardGameModel=new MagicalCardGameModel();
+
+    System.out.println("Welcome to Magical Card Game\nSelect a card")
+
+
 }
--- src/MagicalCardGameModel.java
+++ src/MagicalCardGameModel.java
@@ -5,4 +5,52 @@ public class MagicalCardGameModel {
     String[][]firstShuffle=null;
     String[][]secShuffle=null;
     String guessedCard;
+
+    public String getFirstShuffleColPos() {
+        return firstShuffleColPos;
+    }
+
+    public void setFirstShuffleColPos(String firstShuffleColPos) {
+        this.firstShuffleColPos = firstShuffleColPos;
+    }
+
+    public String getSecShuffleColPos() {
+        return secShuffleColPos;
+    }
+
+    public void setSecShuffleColPos(String secShuffleColPos) {
+        this.secShuffleColPos = secShuffleColPos;
+    }
+
+    public String[][] getCardList() {
+        return cardList;
+    }
+
+    public void setCardList(String[][] cardList) {
+        this.cardList = cardList;
+    }
+
+    public String[][] getFirstShuffle() {
+        return firstShuffle;
+    }
+
+    public void setFirstShuffle(String[][] firstShuffle) {
+        this.firstShuffle = firstShuffle;
+    }
+
+    public String[][] getSecShuffle() {
+        return secShuffle;
+    }
+
+    public void setSecShuffle(String[][] secShuffle) {
+        this.secShuffle = secShuffle;
+    }
+
+    public String getGuessedCard() {
+        return guessedCard;
+    }
+
+    public void setGuessedCard(String guessedCard) {
+        this.guessedCard = guessedCard;
+    }
 }
