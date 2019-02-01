537f1b3668ccffa7e14b6c84636e2ed10bdfef96
emilabz <emilabrahamz@gmail.com>
2019-01-31 21:30:24 -0500

    changes for debugging

--- src/com/lambton/magicalcardgame/MCardGame.java
+++ src/com/lambton/magicalcardgame/MCardGame.java
@@ -10,21 +10,31 @@ public class MCardGame {
 
         String[][] cardMatrix={{"A","4","3"},{"K","7","2"},{"5","9","8"}};
         magicalCardGameModel.setCardList(cardMatrix);
-
+        System.out.println("list:"+magicalCardGameModel.getCardList());
         System.out.println("Welcome to Magical Card Game\nSelect a card");
         String selc=sc.nextLine();
 
         System.out.println("Select the column position of your card");
         magicalCardGameModel.setFirstShuffleColPos(sc.nextLine());
-        magicalCardGameModel.setFirstShuffle(mc.getFirstShuffleResult(magicalCardGameModel));
+        System.out.println("pos sel:"+magicalCardGameModel.getFirstShuffleColPos());
+        String[][] temp1=mc.getFirstShuffleResult(magicalCardGameModel);
+        magicalCardGameModel.setFirstShuffle(temp1);
+        //magicalCardGameModel.setFirstShuffle(mc.getFirstShuffleResult(magicalCardGameModel));
         System.out.println("output for first shuffle:\n"+magicalCardGameModel.getFirstShuffle());
-
+//----------------------------------------------------------------------------------------------------------
         System.out.println("Now select the column position of your card");
         magicalCardGameModel.setSecShuffleColPos(sc.nextLine());
         magicalCardGameModel.setSecShuffle(mc.getSecShuffleResult(magicalCardGameModel));
-        System.out.println("output for first shuffle:\n"+magicalCardGameModel.getSecShuffle());
+        String[][] temp2= magicalCardGameModel.getSecShuffle();
+        System.out.println("output for first shuffle:\n");
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+                System.out.println(temp2[i][j]+" ");
+            
+        }
 
         magicalCardGameModel.setGuessedCard(mc.getFinalResult(magicalCardGameModel));
-        System.out.println("Selected card is"+magicalCardGameModel.getGuessedCard());
+        System.out.println("Selected card is "+magicalCardGameModel.getGuessedCard());
     }
 }
--- src/com/lambton/magicalcardgame/MagicalCardGameCalculator.java
+++ src/com/lambton/magicalcardgame/MagicalCardGameCalculator.java
@@ -1,12 +1,12 @@
 package com.lambton.magicalcardgame;
-import java.util.Scanner;
 import java.lang.*;
 
 public class MagicalCardGameCalculator implements MagicalCardGameInterface{
     int i;
     public String[][] getFirstShuffleResult(MagicalCardGameModel magicalCardGameModel){
         String[][] a=magicalCardGameModel.getCardList();
-        String[][] b=null;
+        String[][] b={{"0","0","0"},{"0","0","0"},{"0","0","0"}};
+        System.out.println("inside fun1");
         //if(magicalCardGameModel.getFirstShuffleColPos()=="C1"){
         if(magicalCardGameModel.getFirstShuffleColPos().equals("C1")){
             for(i=0;i<3;i++){
@@ -17,7 +17,12 @@ public class MagicalCardGameCalculator implements MagicalCardGameInterface{
         }
         //else if(magicalCardGameModel.getFirstShuffleColPos()=="C2"){
         else if(magicalCardGameModel.getFirstShuffleColPos().equals("C2")){
+            //System.out.println("inside else");
             for(i=0;i<3;i++){
+                //System.out.println("before"+b[0][i]);
+//                b[0][i].concat(a[i][0]);
+//               b[1][i].concat(a[i][1]);
+//               b[2][i].concat(a[i][2]);
                 b[0][i]=a[i][0];
                 b[1][i]=a[i][1];
                 b[2][i]=a[i][2];
@@ -34,7 +39,7 @@ public class MagicalCardGameCalculator implements MagicalCardGameInterface{
     }
     public String[][] getSecShuffleResult(MagicalCardGameModel magicalCardGameModel){
         String[][] a=magicalCardGameModel.getFirstShuffle();
-        String[][] b=null;
+        String[][] b={{"0","0","0"},{"0","0","0"},{"0","0","0"}};
         if(magicalCardGameModel.getSecShuffleColPos()=="C1"){
             for(i=0;i<3;i++){
                 b[0][i]=a[i][1];
