08af5bff2ad390ecb9d03903ab6d1510c4123cce
emilabz <emilabrahamz@gmail.com>
2019-01-31 20:07:06 -0500

    New changes in the main class n logic

--- src/com/lambton/magicalcardgame/MCardGame.java
+++ src/com/lambton/magicalcardgame/MCardGame.java
@@ -4,13 +4,24 @@ import java.util.Scanner;
 import java.lang.*;
 public class MCardGame {
     public static void main(String args[]){
-        /*Scanner sc=new Scanner(System.in);
-        System.out.println("Welcome to Magical Card Game\nSelect a card")
-        String selc=sc.nextLine();
-        System.out.println("Select the column position of your")*/
-        String[][] cardMatrix={{"A","4","3"},{"K","7","2"},{"5","9","8"}};
+        Scanner sc=new Scanner(System.in);
         MagicalCardGameModel magicalCardGameModel=new MagicalCardGameModel();
-        MagicalCardGameCalculator cal= new MagicalCardGameCalculator();
+        MagicalCardGameCalculator mc=new MagicalCardGameCalculator();
+
+        String[][] cardMatrix={{"A","4","3"},{"K","7","2"},{"5","9","8"}};
         magicalCardGameModel.setCardList(cardMatrix);
+
+        System.out.println("Welcome to Magical Card Game\nSelect a card");
+        String selc=sc.nextLine();
+
+        System.out.println("Select the column position of your card");
+        magicalCardGameModel.setFirstShuffleColPos(sc.nextLine());
+        mc.getFirstShuffleResult(magicalCardGameModel);
+
+        System.out.println("Now select the column position of your card");
+        magicalCardGameModel.setSecShuffleColPos(sc.nextLine());
+        mc.getSecShuffleResult(magicalCardGameModel);
+
+        
     }
 }
--- src/com/lambton/magicalcardgame/MagicalCardGameCalculator.java
+++ src/com/lambton/magicalcardgame/MagicalCardGameCalculator.java
@@ -1,10 +1,19 @@
 package com.lambton.magicalcardgame;
+import java.util.Scanner;
+import java.lang.*;
 
 public class MagicalCardGameCalculator implements MagicalCardGameInterface{
+    int i,j;
     public String[][] getFirstShuffleResult(MagicalCardGameModel magicalCardGameModel){
         String[][] a=magicalCardGameModel.getCardList();
+        String[][] b=null;
         if(magicalCardGameModel.getFirstShuffleColPos()=="C1"){
-            for()
+            for(i=0;i<3;i++){
+                b[0][i]=a[i][1];
+                b[1][i]=a[i][0];
+                b[2][i]=a[i][2];
+            }
+            magicalCardGameModel.
         }
         else if(magicalCardGameModel.getFirstShuffleColPos()=="C2"){
 
